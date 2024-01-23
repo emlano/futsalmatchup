2984a0983fc1a91925fe93a1c63816c7ad2bff56
emilano <emlano.git@gmail.com>
2024-01-23 21:29:57 +0530

    feat: add user classes
    This adds ability to create User objects: Player and Manager.

---
+++ frontend/lib/users/manager.dart
@@ -0,0 +1,8 @@
+import 'package:frontend/users/user.dart';
+
+class Manager extends User {
+  Manager(String userId, String username, String password, String phoneNo)
+      : super(userId, username, password, phoneNo);
+
+  Manager.fromMap(Map<String, dynamic> map) : super.fromMap(map);
+}
