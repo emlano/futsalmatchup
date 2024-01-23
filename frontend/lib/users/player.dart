6abbd724871d7b4d645000ccc164479b246c37c2
emilano <emlano.git@gmail.com>
2024-01-23 22:37:58 +0530

    feat & test: add library classes User, Player and Manager (#23)
    * feat: add user classes
    
    This adds ability to create User objects: Player and Manager.
    
    * test: user classes, fix: equality checks for user class
    
    Added unit testing for user classes and replaced the equals method with a dart standard way of checking equality.

---
+++ frontend/lib/users/player.dart
@@ -0,0 +1,44 @@
+import 'package:frontend/users/user.dart';
+
+class Player extends User {
+  double skill;
+  double sportsmanship;
+  double overall;
+  String city;
+  bool availability;
+
+  Player(
+      String userId,
+      String username,
+      String password,
+      String phoneNo,
+      this.skill,
+      this.sportsmanship,
+      this.overall,
+      this.city,
+      this.availability)
+      : super(userId, username, password, phoneNo);
+
+  @override
+  Player.fromMap(Map<String, dynamic> map)
+      : sportsmanship = map['sportsmanship'],
+        overall = map['overall'],
+        city = map['city'],
+        availability = map['availability'],
+        skill = map['skill'],
+        super.fromMap(map);
+
+  @override
+  Map<String, dynamic> toMap() {
+    Map<String, dynamic> map = super.toMap();
+    map.addAll({
+      'skill': skill,
+      'sportsmanship': sportsmanship,
+      'overall': overall,
+      'city': city,
+      'availability': availability
+    });
+
+    return map;
+  }
+}
---
+++ frontend/test/user_test.dart
@@ -0,0 +1,55 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:frontend/users/manager.dart';
+import 'package:frontend/users/player.dart';
+
+void main() {
+  test("Test User methods", () {
+    String id = '1234';
+    String username = 'emilano';
+    String password = 'password';
+    String phoneNo = '123456789';
+    double sport = 19.99;
+    double skill = 20.01;
+    double overall = 25.55;
+    String city = 'Colombo';
+    bool aval = true;
+
+    // Create maps for a player and a manager
+    Map<String, dynamic> playerMap = {
+      'userId': '${id}5',
+      'username': username,
+      'password': password,
+      'phoneNo': phoneNo,
+      'sportsmanship': sport,
+      'overall': overall,
+      'skill': skill,
+      'city': city,
+      'availability': aval
+    };
+
+    Map<String, dynamic> managerMap = {
+      'userId': '${id}6',
+      'username': username,
+      'password': password,
+      'phoneNo': phoneNo
+    };
+
+    Player p = Player('${id}5', username, password, phoneNo, skill, sport,
+        overall, city, aval);
+
+    Player pClone = Player.fromMap(playerMap);
+
+    Manager m = Manager('${id}6', username, password, phoneNo);
+
+    Manager mClone = Manager.fromMap(managerMap);
+
+    // Test if both constructors create identical objs
+    expect(p == pClone, true);
+    // Test if the map returned by the object is the same as the
+    // used to build it
+    expect(p.toMap(), playerMap);
+
+    expect(m == mClone, true);
+    expect(m.toMap(), managerMap);
+  });
+}
