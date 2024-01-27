e484ebbf4f311150a9d76d6db787c18d7f6aa0c1
emilano <emlano.git@gmail.com>
2024-01-27 20:59:41 +0530

    fix: spelling in Title

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
         useMaterial3: true,
       ),
-      home: const LoginScreen(title: 'Logic Screen'),
+      home: const LoginScreen(title: 'Login Screen'),
     );
   }
 }
