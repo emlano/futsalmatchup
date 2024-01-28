692befc3c573ab8151b5d6fc79a226c437ab539f
emilano <emlano.git@gmail.com>
2024-01-28 23:48:23 +0530

    feat: created login screen header
    Created a color scheme to go with and added a custom font to be displayed

--- frontend/lib/login_screen.dart
+++ frontend/lib/login_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -14,8 +16,19 @@ class LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.teal,
+        centerTitle: true,
+        title: Text(
+          widget.title,
+          style: const TextStyle(
+            fontSize: 32,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 0,
+            wordSpacing: 5,
+            fontFamily: 'DancingScript',
+          ),
+        ),
       ),
     );
   }
--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
         useMaterial3: true,
       ),
-      home: const LoginScreen(title: 'Login Screen'),
+      home: const LoginScreen(title: 'Futsal MatchUp'),
     );
   }
 }
