dd76bdc0dc15e5644e4e35830075b26a8925507f
emilano <emlano.git@gmail.com>
2024-02-03 17:36:53 +0530

    feat: adds assets folder

--- frontend/lib/login_screen.dart
+++ frontend/lib/login_screen.dart
@@ -34,22 +34,22 @@ class LoginScreenState extends State<LoginScreen> {
             size: 35,
           )),
       body: Column(children: [
-        const SizedBox(height: 50,),
+        const SizedBox(
+          height: 50,
+        ),
         CarouselSlider(
           options: CarouselOptions(
-              height: 450.0,
-            autoPlay: true,
-            autoPlayCurve: Curves.fastEaseInToSlowEaseOut,
-            enlargeCenterPage: true,
-            enlargeFactor: 0.55
-          ),
+              height: 300,
+              autoPlay: true,
+              autoPlayCurve: Curves.fastEaseInToSlowEaseOut,
+              enlargeCenterPage: true,
+              enlargeFactor: 1),
           items: [0, 1, 2].map((i) {
             return Builder(
               builder: (BuildContext context) {
                 return Container(
-                    width: 200,
-                    margin: const EdgeInsets.symmetric(horizontal: 5.0),
-                    decoration: const BoxDecoration(color: Colors.white),
+                  margin: const EdgeInsets.symmetric(horizontal: 5.0),
+                  decoration: const BoxDecoration(color: Colors.white),
                   child: Column(
                     children: [
                       CarouselData.images[i],
@@ -72,26 +72,25 @@ class CarouselData {
   static final List<Image> images = [
     'assets/images/img1.jpg',
     'assets/images/img2.png',
-    'assets/images/img3.png']
-      .map((i) { return Image.asset(
-      i, width: 200, height: 200,
-
-  ); }).toList();
-  
-  static final List<Text> texts = [
-    'Find Players',
-    'Find Stadiums',
-    'Play Futsal'
+    'assets/images/img3.png'
   ].map((i) {
+    return Image.asset(
+      i,
+      width: 200,
+      height: 200,
+    );
+  }).toList();
+
+  static final List<Text> texts =
+      ['Find Players', 'Find Stadiums', 'Play Futsal'].map((i) {
     return Text(
       i,
       textAlign: TextAlign.center,
       style: const TextStyle(
-        color: Colors.teal,
-        fontWeight: FontWeight.bold,
-        fontFamily: 'Inter',
-        fontSize: 20,
-      ),
+          color: Colors.teal,
+          fontFamily: 'Lobster',
+          fontSize: 26,
+          wordSpacing: 5),
     );
   }).toList();
 }
