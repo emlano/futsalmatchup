e2de60936d562437023cc3ae4b921f65031191c4
emilano <emlano.git@gmail.com>
2024-02-03 19:39:29 +0530

    fix: image overflowing container, fix: images use too much memory

--- frontend/lib/login_screen.dart
+++ frontend/lib/login_screen.dart
@@ -1,6 +1,6 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
-import 'package:frontend/IconFonts/IconFonts.dart';
+import 'package:frontend/IconFonts/icon_fonts.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key, required this.title});
@@ -15,62 +15,63 @@ class LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-          backgroundColor: Colors.white,
-          foregroundColor: Colors.teal,
-          centerTitle: true,
-          title: Text(
-            widget.title,
-            style: const TextStyle(
-              fontSize: 32,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 0,
-              wordSpacing: 5,
-              fontFamily: 'DancingScript',
+        appBar: AppBar(
+            backgroundColor: Colors.white,
+            foregroundColor: Colors.teal,
+            centerTitle: true,
+            title: Text(
+              widget.title,
+              style: const TextStyle(
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 0,
+                wordSpacing: 5,
+                fontFamily: 'DancingScript',
+              ),
             ),
-          ),
-          leading: const Icon(
-            IconFont.soccer_ball,
-            size: 35,
-          )),
-      body: Column(children: [
-        const SizedBox(
-          height: 50,
-        ),
-        CarouselSlider(
-          options: CarouselOptions(
-              height: 300,
-              autoPlay: true,
-              autoPlayCurve: Curves.fastEaseInToSlowEaseOut,
-              enlargeCenterPage: true,
-              enlargeFactor: 1),
-          items: [0, 1, 2].map((i) {
-            return Builder(
-              builder: (BuildContext context) {
-                return Container(
-                  margin: const EdgeInsets.symmetric(horizontal: 5.0),
-                  decoration: const BoxDecoration(color: Colors.white),
-                  child: Column(
-                    children: [
-                      CarouselData.images[i],
-                      const SizedBox(height: 10),
-                      CarouselData.texts[i]
-                    ],
-                  ),
+            leading: const Icon(
+              IconFont.soccerBall,
+              size: 35,
+            )),
+        body: Column(children: [
+            const SizedBox(
+              height: 50,
+            ),
+            CarouselSlider(
+              options: CarouselOptions(
+                  height: 300,
+                  autoPlay: true,
+                  autoPlayCurve: Curves.fastEaseInToSlowEaseOut,
+                  enlargeCenterPage: true,
+                  enlargeFactor: 0.5),
+              items: [0, 1, 2].map((i) {
+                return Builder(
+                  builder: (BuildContext context) {
+                    return Container(
+                      margin: const EdgeInsets.symmetric(horizontal: 5.0),
+                      decoration: const BoxDecoration(color: Colors.transparent),
+                      child: FittedBox(child: Column(
+                        children: [
+                          CarouselData.images[i],
+                          const SizedBox(height: 10),
+                          CarouselData.texts[i]
+                        ],
+                      ),
+                      )
+                    );
+                  },
                 );
-              },
-            );
-          }).toList(),
-        ),
-        const SizedBox(height: 25),
-      ]),
-    );
+              }).toList(),
+            ),
+            const SizedBox(height: 25),
+          ]),
+        );
   }
 }
 
 class CarouselData {
   static final List<Image> images = [
-    'assets/images/img1.jpg',
+    'assets/images/img1.png',
     'assets/images/img2.png',
     'assets/images/img3.png'
   ].map((i) {
@@ -78,6 +79,7 @@ class CarouselData {
       i,
       width: 200,
       height: 200,
+      isAntiAlias: true,
     );
   }).toList();
 
