f410820c6a2bc151bcd435ff1c5fd0e378a66847
rachelcooray <rachelcooray@gmail.com>
2024-02-06 16:45:57 +0530

    Updated the file path of the app icon

--- frontend/lib/views/stadium/stadium_availability_page.dart
+++ frontend/lib/views/stadium/stadium_availability_page.dart
@@ -47,7 +47,7 @@ class _StadiumAvailabilityPageState extends State<StadiumAvailabilityPage> {
         title: Row(
           children: [
             Image.asset(
-              'assets/app_logo.png',
+              'assets/icons/app_icon.png',
               width: 40,
               height: 40,
             ),
