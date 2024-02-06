790b2192d2e2875f9c82251e3b5dfbca1b96fb3d
rachelcooray <rachelcooray@gmail.com>
2024-02-06 22:40:31 +0530

    Added a test for creating a team

---
+++ frontend/test/create_team_test.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:frontend/views/team/create_team_page.dart';
+
+void main() {
+  testWidgets('CreateTeamPage UI Test', (WidgetTester tester) async {
+    await tester.pumpWidget(const MaterialApp(home: CreateTeamPage()));
+
+    // Verify the presence of widgets on CreateTeamPage
+    expect(find.text('Create Your Team'), findsOneWidget);
+    expect(find.text('Add Team Logo'), findsOneWidget);
+    expect(find.text('Enter your Team Name'), findsOneWidget);
+    expect(find.text('Create Team'), findsOneWidget);
+  });
+}
