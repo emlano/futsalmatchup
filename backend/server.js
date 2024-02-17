abe031e6a9da299b186e755b806d54f34c70df1c
emilano <emlano.git@gmail.com>
2024-02-18 00:00:01 +0530

    feat: found way to access mysql db through api

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,33 @@
-const express = require("express");
-const app = express();
-const port = 3000;
+require("dotenv").config()
+const express = require("express")
+const mysql = require("mysql2")
 
-app.use(express.json());
+const port = 3000
+
+const app = express()
+app.use(express.json())
+
+const pool = mysql.createPool({
+    host: process.env.HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
+}).promise()
+
+async function getUsers() {
+    const [rows] = await pool.query("SELECT * FROM `players`;")
+    return rows
+}
 
 app.listen(port, () => {
-    console.log(`Connected at http://localhost:${port}`);
+    console.log(`Connected at http://localhost:${port}`)
 })
 
 const userRouter = require('./routes/users')
 
-app.use('/users', userRouter)
+
+app.get('/users', (req, res) => {
+    getUsers().then(x => {
+        res.json(x)
+    })
+})
