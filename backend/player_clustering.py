79d98970689c74e8523e7c449a8254b6635b090c
rachelcooray <rachelcooray@gmail.com>
2024-02-24 09:50:41 +0530

    Split the data as training, testing

--- backend/player_clustering.py
+++ backend/player_clustering.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
 
 # Load the dataset
 dataset = pd.read_csv("fifa_players.csv")
@@ -33,12 +36,27 @@ if missing_values.any():
     # Assigning the median
     # dataset = dataset.fillna(dataset.median(), inplace=True)
 
+# Select the required columns from the dataset
+X = dataset[['age', 'overall_rating']]
+
 # Split the dataset as 70:30 to train and test the model
+X_train, X_test = train_test_split(X, test_size=0.3)
 
 # Train the model using KMeans clustering for the 70% chosen
+# KMeans clustering
+kmeans = KMeans(n_clusters=7) # Choose the value of k
+kmeans.fit(X_train)
 
 # Plot the clusters
+plt.scatter(X_train['age'], X_train['overall_rating'], c=kmeans.labels_, cmap='viridis')
+plt.xlabel('Age')
+plt.ylabel('Overall Rating')
+plt.title('K Means Clustering - Futsal MatchUp')
+plt.show()
 
 # Choose the value of k
 
 # Test the model accuracy with the testing metrics from the 30% chosen
+
+# Predict cluster labels for a value in the test data
+kmeans_predict = kmeans.predict(X_test)
