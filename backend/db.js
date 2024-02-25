56795ba1f49a546f8b01894c59aef5701c134677
rachelcooray <rachelcooray@gmail.com>
2024-02-25 16:25:01 +0530

    Added database configuration

--- backend/db.js
+++ backend/db.js
@@ -1,2 +1,9 @@
 require("dotenv").config();
 const mysql = require("mysql2");
+
+const pool = mysql.createPool({
+    host: process.env.HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
+}).promise();
