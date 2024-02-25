9b5d7b4c048bb05a465c3928d9d2598ca0220c80
rachelcooray <rachelcooray@gmail.com>
2024-02-25 16:14:10 +0530

    Added the endpoint to retrieve all bookings

--- backend/routes/bookings.js
+++ backend/routes/bookings.js
@@ -1,3 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../db");
+
+//Get all bookings
+router.get('/', (req, res) => {
+    db.getBookings.then(bookings => {
+        res.json(bookings);
+    })
+});
