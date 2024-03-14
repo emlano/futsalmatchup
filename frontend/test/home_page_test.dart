ac721ccf1e4d3aaa11a36700d142c02519a81f40
emilano <emlano.git@gmail.com>
2024-03-14 13:51:55 +0530

    chore: refractor backend into node/ and flask/

--- frontend/test/home_page_test.dart
+++ frontend/test/home_page_test.dart
@@ -3,13 +3,13 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:frontend/views/home/Home.dart';
 
 void main() {
-  // testWidgets('Homepage UI Test', (WidgetTester tester) async {
-  //   await tester.pumpWidget(const MaterialApp(home: Home()));
+  testWidgets('Homepage UI Test', (WidgetTester tester) async {
+    await tester.pumpWidget(const MaterialApp(home: Home()));
 
-  //   // Verify the presence of widgets on CreateTeamPage
-  //   expect(find.text('Upcoming Bookings'), findsOneWidget);
-  //   expect(find.text('joined teams'), findsOneWidget);
-  //   expect(find.text('invite requests'), findsOneWidget);
-  //   expect(find.text('menu to lead to other pages'), findsOneWidget);
-  // });
+    // Verify the presence of widgets on CreateTeamPage
+    expect(find.text('Upcoming Bookings'), findsOneWidget);
+    expect(find.text('joined teams'), findsOneWidget);
+    expect(find.text('invite requests'), findsOneWidget);
+    expect(find.text('menu to lead to other pages'), findsOneWidget);
+  });
 }
